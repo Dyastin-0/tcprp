57a4a600dca97a7208a7bb27e254ab02317753b2
Justine Paralejas <paralejasjustine15@gmail.com>
2025-10-07 21:52:02 +0800

    pprof

--- tcprp.go
+++ tcprp.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
 	"os"
+
+	_ "net/http/pprof"
 
 	"github.com/Dyastin-0/tcprp/cmd"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	command := cmd.New()
 
 	if err := command.Run(context.Background(), os.Args); err != nil {
